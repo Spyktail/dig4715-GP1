bdfeed2e9dddc3c9a72bc8537507871278561b8b
Spyktail <rad326@knights.ucf.edu>
2022-02-12 12:39:50 -0500

    andrew commit

---
+++ Assets/Scripts/DuckBehavior.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckBehavior : MonoBehaviour
+{
+    public float speed;
+    public float minXval;
+    public float maxXval;
+    public float maxYval;
+    Vector3 target;
+
+    public float timeActive;
+    Animator anim;
+    SpriteRenderer spriteRend;
+    Rigidbody2D rb;
+    bool isDead;
+    bool startedFalling;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Vector3.Distance(transform.position, target) > 0)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            }
+            else
+            {
+                if (transform.position.x > maxXval || transform.position.x < minXval || transform.position.y > maxYval)
+                {
+                    Destroy(gameObject);
+                }
+
+                SetTarget();
+
+            }
+
+            if (timeActive > 0)
+            {
+                timeActive -= Time.deltaTime;
+            }
+
+            UpdateSprite();
+        }
+        else
+        {
+            if (startedFalling)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, transform.position - transform.up, 10*Time.deltaTime);
+                }
+        }
+    }
+
+     public void Timeup()
+    {
+        speed *= 2;
+        target = transform.position + new Vector3(0, 20, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isDead)
+            StartCoroutine(Dead());
+    }
+
+    IEnumerator Dead()
+    {
+        HunterController.Instance.HitDuck();
+        isDead = true;
+        anim.SetTrigger("Die");
+        yield return new WaitForSeconds(0.4f);
+        startedFalling = true;
+        Destroy(gameObject, 3f);
+    }
+
+    public void UpdateSprite()
+        {
+            if (transform.position.x - target.x > 0)
+            {
+                transform.localScale = new Vector3(-0.7f, 0.7f, 0.7f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+            }
+
+            if (Mathf.Abs(transform.position.x - target.x) < 1)
+            {
+                anim.SetInteger("Fly", 2);
+            }
+            else if (Mathf.Abs(transform.position.y - target.y) < 1)
+            {
+                anim.SetInteger("Fly", 0);
+            }
+            else
+            {
+                anim.SetInteger("Fly", 1);
+            }
+        }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SetTarget();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, target);
+    }
+    public void SetTarget()
+    {
+        if (timeActive > 0)
+        {
+            target = target + new Vector3(Random.Range(-20, 20), Random.Range(-3, 7), 0);
+            if (target.x > 20)
+            {
+                target.x = 20;
+            }
+                
+            if (target.x < -20)
+            {
+                target.x = -20;
+            }
+            if (target.y > 7)
+            {
+                target.y = 7;
+            }
+
+            if (target.y < -2)
+            {
+                target.y = 2;
+            }
+        }
+    }
+}
+        
